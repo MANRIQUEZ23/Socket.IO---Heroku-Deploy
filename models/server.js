2a211bbd323dccb20ca605984bd31bf7c2c5817c
Carlos Manriquez <camb23@gmail.com>
2022-12-27 09:04:00 -0800

    Path del socket server actualizado

--- models/server.js
+++ models/server.js
@@ -5,6 +5,8 @@ const http = require('http');
 const socketio = require('socket.io');
 
 const path = require('path');
+const cors = require('cors');
+
 const Sockets = require('./sockets'); 
  
 
@@ -24,6 +26,9 @@ class Server{
 
     middlewares(){
         this.app.use( express.static( path.resolve(  __dirname  , '../public' )  ));
+
+        // CORS
+        this.app.use( cors );
     }
 
 
